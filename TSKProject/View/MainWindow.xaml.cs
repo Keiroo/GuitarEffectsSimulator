eeef3b8fe9f61210f97d26d90ba467fd31d2dfee
Keiroo <orynia55@o2.pl>
2018-10-20 17:56:31 +0200

    Initial commit

---
+++ TSKProject/View/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TSKProject.ViewModel;
+
+namespace TSKProject.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOpenFileClick(object sender, RoutedEventArgs e)
+        {
+            MainWindowViewModel vm = MainGrid.DataContext as MainWindowViewModel;
+            vm.OnOpenFileClick();
+        }
+    }
+}
---
+++ TSKProject/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSKProject.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public string FileName { get; set; }
+
+        public MainWindowViewModel()
+        {
+            FileName = "File Not Loaded";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnOpenFileClick()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            var res = fileDialog.ShowDialog();
+
+            if (res == true)
+            {
+                FileName = fileDialog.FileName;
+                OnPropertyChanged("FileName");
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
